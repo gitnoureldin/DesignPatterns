120fd41b788407962762ec99c9250890f9706931
Noureldin Elabyad <Noureldin.Elabyad@thinfabrics.com>
2024-05-13 15:45:52 +0200

    classic factory pattern

---
+++ FactoryPattern/CarFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.ConstrainedExecution;
+using System.Security.AccessControl;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dsignPatterns.FactoryPattern
+{
+    class CarFactory
+    {
+
+        public Car getYourCar(string carType)
+        {
+            switch (carType)
+            {
+                case "s":
+                    return new SedanCar();
+
+                case "u":
+                    return new SuvCar();
+
+
+                case "t":
+                   return new TruckCar();
+
+                default:
+                    throw new ArgumentException("Invalid car type specified"); // to make sure all pathes return the rigth value
+
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,57 +11,15 @@ namespace singelton
     {
         static void Main(string[] args)
         {
-            //class1 c = class1.getIntance();       // we dont write a new class but refare the the methode getInctance and it returns new class every time
-            //c.print("Hello u class1!");
-
-            Parallel.Invoke(    // .Invok to invok the methodes inside
-                () => method1(),
-                () => method2()
-
-                );
-
-            //class1 c2 = class1.getIntance();
-            //c.print("new msg class2");
-
-            //class1.son sn = new class1.son();
-            //sn.print("son class");
-
-
-           // Console.ReadKey(true);
-
-                             //////////////////////////////////////////////////////////////////////////////////////
-                             
-             ///////////
-            // the old way to use factory desig pattern by using Singelton.Factory; 
-           /////////
-
-           // Car car = new SedanCar();
-            //car.RunCar();
-
+           
             Car car = null;
 
-
             Console.WriteLine("select Your Car type,\n s = Sedan,\n U = SUV,\n T= Truck,\n");
-            string CarType = Console.ReadLine();
+            string carType = Console.ReadLine();
 
-            switch (CarType)
-            {
-                case "s":
-                       car = new SedanCar();
-                break ;
-                 
-                case "u":
-                    car = new SuvCar();
+            car = (new CarFactory()).getYourCar(carType);
 
-                break ;
-                 
-                case "t":
-                    car = new TruckCar();
-
-                break ;
-
-            }
-
+        
             runCommand(car);
 
             Console.ReadKey(true);
@@ -73,26 +31,6 @@ namespace singelton
             car.RunCar();
             car.ShowCar();
         }
-
-
-                           /// /////////////////////////////////////////////////////////////////////////////////////////////
-                                   
-      
-        
-        public static void method1()
-        {
-            class1 c = class1.getIntance();       // we dont write a new class but refare the the methode getInctance and it returns new class every time
-            c.print("Hello u class1!");
-
-        }
-        public static void method2()
-        {
-            class1 c2 = class1.getIntance();
-            c2.print("new msg class2");
-        }
-       
-        
-
 
     }
 
